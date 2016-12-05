71c02bc265e98a155aacb93d96d34314d23e1306
ian Wilcox <ian Wilcox>
2016-12-04 19:57:01 -0800

    can add products to db and see product name

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using GBK.Models;
+
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,14 +12,23 @@ namespace GBK.Controllers
 {
     public class ProductsController : Controller
     {
+        private GBKDbContext db = new GBKDbContext();
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            return View(db.Products.ToList());
         }
         public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
         public IActionResult Details()
         {
