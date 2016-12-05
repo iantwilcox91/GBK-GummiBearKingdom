4904d7827ee326d585cfbf016e318b8865c8e965
ian Wilcox <ian Wilcox>
2016-12-04 19:22:16 -0800

    can retrieve data from DataBase on Posts page

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using GBK.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,6 +11,7 @@ namespace GBK.Controllers
 {
     public class BlogController : Controller
     {
+        private GBKDbContext db = new GBKDbContext();
         // GET: /<controller>/
         public IActionResult Index()
         {
@@ -21,7 +23,7 @@ namespace GBK.Controllers
         }
         public IActionResult Posts()
         {
-            return View();
+            return View(db.Blogs.ToList());
         }
     }
 }
